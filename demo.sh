2a29c60c37a02ec4465950b043912be9fc4e8000
bryce-klinker <bklinker@pillartechnology.com>
2016-09-22 15:58:47 -0500

    Update demo to be better :

--- demo.sh
+++ demo.sh
@@ -2,6 +2,8 @@
 
 source ENV/bin/activate
 
+pip install -r requirements.txt --upgrade
+
 applicantstack candidates homeinstead540 >> page1_results.txt
 
 applicantstack candidates homeinstead540 --page 17 >> page17_results.txt
