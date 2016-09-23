527f069f6822f3fce66ccc4d730ad9eeeeab0447
bryce-klinker <bklinker@pillartechnology.com>
2016-09-23 13:32:39 -0500

    Updating demo to include franchises

--- demo.sh
+++ demo.sh
@@ -4,6 +4,8 @@ source ENV/bin/activate
 
 pip install -r requirements.txt --upgrade
 
-applicantstack candidates homeinstead540 >> page1_results.txt
+applicantstack candidates homeinstead540 >> page1_candidates.txt
 
-applicantstack candidates homeinstead540 --page 17 >> page17_results.txt
+applicantstack candidates homeinstead540 --page 17 >> page17_candidates.txt
+
+applicantstack franchises >> franchises.txt
