133432b001a401d693863fdea50689e8621fb4ef
bryce-klinker <bklinker@pillartechnology.com>
2016-09-28 12:38:44 -0500

    added configs for other scenarios

--- demo.sh
+++ demo.sh
@@ -4,8 +4,8 @@ source ENV/bin/activate
 
 pip install -r requirements.txt --upgrade
 
-applicantstack candidates homeinstead540 >> page1_candidates.txt
+# applicantstack candidates homeinstead540 >> page1_candidates.txt
 
-applicantstack candidates homeinstead540 --page 17 >> page17_candidates.txt
+# applicantstack candidates homeinstead540 --page 17 >> page17_candidates.txt
 
-applicantstack franchises >> franchises.txt
+# applicantstack franchises >> franchises.txt
